4fcfc6c8d41207504c237521514201ed702b00ad
itsus <sahujjwalkumar@gmail.com>
2023-12-28 22:01:08 +0530

    edition

--- Sum.java
+++ Sum.java
@@ -2,7 +2,8 @@ public class Sum {
     public static void main(String[] args) {
         int a = 10;
         int b = 60;
-        int c = a + b;
+        int x = 20;
+        int c = a + b + x;
         System.out.println(c);
     }
 
