3f0639ca6906a587fb7c15e0d350b75b463596bd
itsus <sahujjwalkumar@gmail.com>
2023-12-28 21:40:51 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("Kaise Ho?");
+        System.out.println("Kya haal chaal rehta hai apna?");
+
+    }
+
+}
